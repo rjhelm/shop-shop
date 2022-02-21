6a7521fe6bf62b9cf4c8d7ab35da040ae682323e
rjhelm <rjhelm@github.io>
2022-02-20 19:35:11 -0700

    created product display card for data to be passed intop

--- frontend/src/components/CheckoutForm/Review.js
+++ frontend/src/components/CheckoutForm/Review.js
@@ -1,12 +1,12 @@
-import React from 'react'
-import { Typography, List, ListItem, ListItemText } from '@material-ui/core'
+import React from 'react';
+import { Typography, List, ListItem, ListItemText } from '@material-ui/core';
 
-const Review = ({ checkoutToken }) => {
+const Review = ({ checkoutToken }) => (
     <>
-        <Typography variant="h6" gutterBottom>Order Summary</Typography>
+        <Typography variant="h6" gutterBottom>Order summary</Typography>
         <List disablePadding>
             {checkoutToken.live.line_items.map((product) => (
-                <ListItem style={{ padding: "10px 0" }} key={product.name}>
+                <ListItem style={{ padding: '10px 0' }} key={product.name}>
                     <ListItemText primary={product.name} secondary={`Quantity: ${product.quantity}`} />
                     <Typography variant="body2">{product.line_total.formatted_with_symbol}</Typography>
                 </ListItem>
@@ -19,4 +19,6 @@ const Review = ({ checkoutToken }) => {
             </ListItem>
         </List>
     </>
-}
+);
+
+export default Review;
